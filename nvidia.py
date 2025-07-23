1b5f2cf7ced8a48cf4a9728b6ca80f13f8f6043a
THEZORACORE <madsskovlundpallisgaard@gmail.com>
2025-07-23 22:28:50 +0200

    Create nvidia.py

---
+++ nvidia.py
@@ -0,0 +1,11 @@
+import os
+import requests
+
+NVIDIA_API_KEY = os.getenv("=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")
+headers = {
+    "Authorization": f"Bearer {=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}",
+    "Content-Type": "application/json"
+}
+
+response = requests.get("https://api.nvidia.com/v1/ai/status", headers=headers)
+print(response.json())
