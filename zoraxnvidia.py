0fe65a8fb97f8e868c978e9f6327cf6ea0720a7a
Mads Pallisgaard Petersen <madsskovlundpallisgaard@gmail.com>
2025-07-24 17:51:52 +0200

    Create zoraxnvidia.py

---
+++ zoraxnvidia.py
@@ -0,0 +1,8 @@
+import os
+
+NVIDIA_API_KEY = os.getenv("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")
+NVIDIA_ENDPOINTS = {
+    "usd": "https://api.nvidia.com/usd",
+    "rtx": "https://api.nvidia.com/rtx",
+    "nemo": "https://api.nvidia.com/nemo",
+}
